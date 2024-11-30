57c34092eca11d82a256841d1b55b4a8a2503643
robostac <res@robostac.co.uk>
2024-11-30 13:54:46 +0000

    update versions

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,19 @@
 #[aoc(day1, part1)]
-pub fn solve_p1(input: &str) -> u64 {
+pub fn part1(input: &str) -> u64 {
     0
 }
 
 #[aoc(day1, part2)]
-pub fn solve_p2(input: &str) -> u64 {
+pub fn part2(input: &str) -> u64 {
     0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(""), 0);
+        assert_eq!(part2(""), 0);
+    }
 }
