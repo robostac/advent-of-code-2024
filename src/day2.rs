3fa4fb267ade5dacd459e5f59e9f10bcfa49f4d6
robostac <res@robostac.co.uk>
2024-12-02 09:27:15 +0000

    improve day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,16 +1,4 @@
-#[aoc_generator(day2)]
-pub fn input_generator(input: &str) -> Vec<Vec<i32>> {
-    input
-        .lines()
-        .map(|l| {
-            l.split_ascii_whitespace()
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .collect()
-}
-
-fn safe(x: &Vec<i32>) -> Option<usize> {
+fn safe(x: &[i32]) -> Option<usize> {
     let diff = (x[1] - x[0]).signum();
     if diff == 0 {
         return Some(0);
@@ -25,52 +13,64 @@ fn safe(x: &Vec<i32>) -> Option<usize> {
 }
 
 #[aoc(day2, part1)]
-pub fn part1(input: &Vec<Vec<i32>>) -> usize {
-    input.iter().filter(|x| safe(&x).is_none()).count()
+pub fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|l| {
+            let v = l
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+            safe(&v).is_none()
+        })
+        .count()
 }
 
 fn safe_skip(x: &Vec<i32>, skip: usize) -> bool {
     let mut idx = 1;
     let mut last = x[0];
     if skip == 0 {
-        idx += 1;
-        last = x[1];
+        return safe(&x[1..]).is_none();
+    } else if skip == 1 {
+        idx = 2;
     }
-    let diff = (x[idx] - last).signum();
+    let diff = (x[idx] - x[0]).signum();
     if diff == 0 {
         return false;
     }
-    loop {
-        if idx == skip {
-            idx += 1;
+    for (i, &val) in x.iter().enumerate().skip(1) {
+        if i == skip {
+            continue;
         }
-        if idx >= x.len() {
-            return true;
-        }
-
-        let v = x[idx] - last;
-        last = x[idx];
+        let v = val - last;
+        last = val;
         if v.abs() > 3 || v.signum() != diff {
             return false;
         }
-        idx += 1;
     }
+    true
 }
 
 #[aoc(day2, part2)]
-pub fn part2(input: &Vec<Vec<i32>>) -> usize {
+pub fn part2(input: &str) -> usize {
     input
-        .iter()
-        .filter(|&x| match safe(x) {
-            None => true,
-            Some(idx) => (idx.saturating_sub(1)..idx + 2).any(|y| safe_skip(&x, y)),
+        .lines()
+        .filter(|l| {
+            let x = l
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+            match safe(&x) {
+                None => true,
+                Some(idx) => (idx.saturating_sub(1)..idx + 2).any(|y| safe_skip(&x, y)),
+            }
         })
         .count()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{input_generator, part1, part2};
+    use super::{part1, part2};
 
     const TESTLIST: &str = "7 6 4 2 1
 1 2 7 8 9
@@ -80,7 +80,7 @@ mod tests {
 1 3 6 7 9";
     #[test]
     fn sample1() {
-        assert_eq!(part1(&input_generator(TESTLIST)), 2);
-        assert_eq!(part2(&input_generator(TESTLIST)), 4);
+        assert_eq!(part1(TESTLIST), 2);
+        assert_eq!(part2(TESTLIST), 4);
     }
 }
