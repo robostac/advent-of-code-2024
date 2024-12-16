a70947bfb802fe33936cba2573a687d69d6ec199
robostac <res@robostac.co.uk>
2024-12-16 12:45:17 +0000

    day16 faster

--- src/day16.rs
+++ src/day16.rs
@@ -3,15 +3,7 @@ fn do_move(p: usize, d: i64) -> usize {
     (p as i64 + d) as usize
 }
 
-#[aoc(day16, part1)]
-pub fn part1(input_struct: &str) -> usize {
-    let mut input: Vec<char> = input_struct.chars().collect();
-    let line_length = input.iter().position(|x| *x == '\n').unwrap() as i64 + 1;
-    let start = input.iter().position(|x| *x == 'S').unwrap();
-    let end = input.iter().position(|x| *x == 'E').unwrap();
-
-    input[start] = '.';
-    input[end] = '.';
+fn make_scores(input: &[char], start: usize, line_length: i64) -> Vec<[usize; 4]> {
     let directions = [1, line_length, -1, -line_length];
     let mut scores = vec![[usize::MAX; 4]; input.len()];
     let mut q = VecDeque::new();
@@ -32,6 +24,19 @@ pub fn part1(input_struct: &str) -> usize {
             }
         }
     }
+    scores
+}
+
+#[aoc(day16, part1)]
+pub fn part1(input_struct: &str) -> usize {
+    let mut input: Vec<char> = input_struct.chars().collect();
+    let line_length = input.iter().position(|x| *x == '\n').unwrap() as i64 + 1;
+    let start = input.iter().position(|x| *x == 'S').unwrap();
+    let end = input.iter().position(|x| *x == 'E').unwrap();
+
+    input[start] = '.';
+    input[end] = '.';
+    let scores = make_scores(&input, start, line_length);
     *scores[end].iter().min().unwrap()
 }
 
@@ -42,28 +47,30 @@ struct Maze<'a> {
 }
 
 impl Maze<'_> {
-    fn dfs(&self, on_path: &mut [[bool; 4]], pos: usize, d: usize, end: usize, cur: usize) -> bool {
-        if self.scores[pos][d] < cur {
+    fn dfs(&self, on_path: &mut [usize], pos: usize, d: usize, end: usize, cur: usize) -> bool {
+        if self.scores[pos][d] != cur {
             return false;
         }
         if self.input[pos] == '#' {
             return false;
         }
-        if on_path[pos][d] {
+        if (on_path[pos] & (1 << d)) > 0 {
             return true;
         }
         if pos == end {
-            on_path[pos][d] = true;
+            on_path[pos] |= 1 << d;
             return true;
         }
         for (np, nd, ns) in [
-            (do_move(pos, self.directions[d]), d, 1),
+            (do_move(pos, -self.directions[d]), d, 1),
             (pos, (d + 1) % 4, 1000),
             (pos, (d + 3) % 4, 1000),
         ] {
-            on_path[pos][d] |= self.dfs(on_path, np, nd, end, cur + ns)
+            if self.dfs(on_path, np, nd, end, cur - ns) {
+                on_path[pos] |= 1 << d;
+            }
         }
-        return on_path[pos][d];
+        return (on_path[pos] & (1 << d)) > 0;
     }
 }
 #[aoc(day16, part2)]
@@ -76,41 +83,19 @@ pub fn part2(input_struct: &str) -> usize {
     input[start] = '.';
     input[end] = '.';
     let directions = [1, line_length, -1, -line_length];
-    let mut scores = vec![[usize::MAX; 4]; input.len()];
-    let mut q = VecDeque::new();
-    q.push_back((start, 0));
-    scores[start][0] = 0;
-    while let Some((p, d)) = q.pop_front() {
-        let cur_score = scores[p][d];
-        for (np, nd, ns) in [
-            (do_move(p, directions[d]), d, 1),
-            (p, (d + 1) % 4, 1000),
-            (p, (d + 3) % 4, 1000),
-        ] {
-            if input[np] == '.' {
-                if cur_score + ns < scores[np][nd] {
-                    q.push_back((np, nd));
-                    scores[np][nd] = cur_score + ns;
-                }
-            }
-        }
-    }
+    let scores = make_scores(&input, start, line_length);
     let bscore = *scores[end].iter().min().unwrap();
-    for (i, p) in input.iter_mut().enumerate() {
-        let bi = *scores[i].iter().min().unwrap();
-        if bi > bscore {
-            *p = '#';
-        }
-    }
-    let mut best = vec![[false; 4]; input.len()];
+    let mut best = vec![0; input.len()];
     let mz = Maze {
         input: &input,
         scores: &scores,
         directions: &directions,
     };
-    mz.dfs(&mut best, start, 0, end, 0);
+    for i in 0..4 {
+        mz.dfs(&mut best, end, i, start, bscore);
+    }
 
-    best.iter().filter(|x| x.iter().any(|y| *y)).count()
+    best.iter().filter(|x| **x > 0).count()
 }
 
 #[cfg(test)]
@@ -138,6 +123,6 @@ mod tests {
     }
     #[test]
     fn sample2() {
-        assert_eq!(part2(TESTLIST), 9021);
+        assert_eq!(part2(TESTLIST), 45);
     }
 }
