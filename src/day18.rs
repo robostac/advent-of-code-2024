b8f8ec23e0650abfa83d21916887d54de3a4732f
robostac <res@robostac.co.uk>
2024-12-18 12:13:35 +0000

    faster day18

--- src/day18.rs
+++ src/day18.rs
@@ -49,30 +49,56 @@ pub fn part1(input_struct: &str) -> i64 {
     solve(71, 71, &parse_in(input_struct)[..1024]).unwrap()
 }
 
-fn binary_search_range_max(l: i64, r: i64, res: &dyn Fn(i64) -> bool) -> i64 {
-    let mut l = l;
-    let mut r = r;
-    if res(r) {
-        return r;
+fn solve2(width: i64, height: i64, bytes: &[(i64, i64)]) -> String {
+    let mut bad = vec![vec![i64::MAX; height as usize]; width as usize];
+    let mut vdq = VecDeque::new();
+    vdq.push_back((0i64, 0i64));
+
+    bad[0][0] = 0;
+    for (x, y) in bytes.iter() {
+        bad[*x as usize][*y as usize] = -1;
     }
-    while (l + 1) < r {
-        let m = l.saturating_add(r) / 2;
-        let v = res(m);
-        if v == true {
-            l = m;
+    let mut ans = bytes.len();
+    loop {
+        if let Some((x, y)) = vdq.pop_front() {
+            let t = bad[x as usize][y as usize];
+
+            if t < 0 {
+                continue;
+            }
+
+            for (nx, ny) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)] {
+                if nx < 0 || nx >= height || ny < 0 || ny >= height {
+                    continue;
+                }
+                let z = bad[nx as usize][ny as usize];
+                if z <= t + 1 {
+                    continue;
+                }
+                bad[nx as usize][ny as usize] = t + 1;
+                if nx == width - 1 && ny == height - 1 {
+                    return format!("{},{}", bytes[ans as usize].0, bytes[ans as usize].1);
+                }
+                vdq.push_back((nx, ny));
+            }
         } else {
-            r = m;
+            ans -= 1;
+            let (x, y) = bytes[ans];
+            bad[x as usize][y as usize] = i64::MAX;
+            for (nx, ny) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)] {
+                if nx < 0 || nx >= height || ny < 0 || ny >= height {
+                    continue;
+                }
+                let z = bad[nx as usize][ny as usize];
+                if z < 0 || z == i64::MAX {
+                    continue;
+                }
+                vdq.push_back((nx, ny));
+            }
         }
     }
-    return l;
 }
 
-fn solve2(width: i64, height: i64, bytes: &[(i64, i64)]) -> String {
-    let ans = binary_search_range_max(0, bytes.len() as i64, &|x| {
-        solve(width, height, &bytes[..(x as usize)]).is_some()
-    });
-    format!("{},{}", bytes[ans as usize].0, bytes[ans as usize].1)
-}
 #[aoc(day18, part2)]
 pub fn part2(input_struct: &str) -> String {
     let b = &parse_in(input_struct);
